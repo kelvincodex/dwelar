97bb659d1703a6e04f46a29e22b1a610f43d071b
keshiecodes <morahthankgod@gmail.com>
2023-02-28 08:37:16 +0100

    jjjjgg

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,6 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-<x-comp.head></x-comp.head>
+<head>
+    <!-- Title -->
+    <title>{{ env("APP_NAME") }}</title>
+
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset("assets-open/favicon.png") }}">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900&amp;display=swap" rel="stylesheet">
+
+    <!-- CSS Implementing Plugins -->
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/font-awesome/css/fontawesome-all.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/css/font-mytravel.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/animate.css/animate.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/hs-megamenu/src/hs.megamenu.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/flatpickr/dist/flatpickr.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/flatpickr/dist/flatpickr.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/bootstrap-select/dist/css/bootstrap-select.min.css")}}">
+
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/slick-carousel/slick/slick.css")}}">
+    <link rel="stylesheet" href="{{asset("assets-open/vendor/fancybox/jquery.fancybox.css")}}">
+
+    <!-- CSS MyTravel Template -->
+    <link rel="stylesheet" href="{{asset("assets-open/css/theme.css")}}">
+</head>
+
 <body>
 <!-- ========== HEADER ========== -->
 <x-comp.header></x-comp.header>
